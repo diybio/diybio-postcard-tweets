28c75876242ecaf5bb375aaba5384fb5fb96b75a
Mackenzie Cowell <mac@has100ideas.com>
2013-10-29 13:24:02 -0700

    increased tweets displayed from 8 to 16 on main page

--- app.rb
+++ app.rb
@@ -12,11 +12,12 @@ options '/tweets.json' do
   halt 200
 end
 
-# limit query to most recent 8 tweets
+# limit query to most recent 16 tweets
 get '/tweets.json' do
   allow_cors
   content_type "application/json"
-  tweets_collection.find.limit(8).sort({tweet_id: -1}).to_a.to_json
+  tweets_collection.find.limit(16).sort({tweet_id: -1}).to_a.to_json
+  # tweets_collection.find({text: {$regex : '.*diybiohi.*'}}).sort({​time: -1}).limit(20)
 end
 
 get '/' do
